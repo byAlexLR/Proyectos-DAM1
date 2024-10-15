c6a3c02116d76a6acfcb02b50cb07c92e075eeeb
Alex Lorenzo <147382513+byAlexLR@users.noreply.github.com>
2024-10-15 16:54:59 +0100

    Add files via upload

---
+++ Programación (PRO)/1. Ejercicios Sencillos - Copy/src/main/java/com/project/ejerciciossencillos/BienvenidaNombre.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+package com.project.ejerciciossencillos;
+
+/**
+ *
+ * @author byAlexLR
+ */
+public class BienvenidaNombre {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args){
+        // TODO code application logic here
+        String nombre = "Alejandro";
+
+        System.out.println("Bienvenido, " + nombre);
+    }
+}
---
+++ Programación (PRO)/1. Ejercicios Sencillos - Copy/src/main/java/com/project/ejerciciossencillos/VariablesNumericas.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+package com.project.ejerciciossencillos;
+
+/**
+ *
+ * @author byAlexLR
+ */
+public class VariablesNumericas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args){
+        int a = 8, b = 5;
+
+        System.out.println("Los numeros utilizados son " + a + " y " + b);
+        System.out.println("Suma: " + (a + b));
+        System.out.println("Resta: " + (a - b));
+        System.out.println("Multiplicacion: " + (a * b));
+        System.out.println("Division: " + (a / b));
+        System.out.println("Modulo: " + (a % b));
+    }
+}
