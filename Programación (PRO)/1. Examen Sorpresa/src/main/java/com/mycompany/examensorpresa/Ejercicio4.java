ffdc3d1204f4bdd7d22891fff3cbef4327e07437
Alex Lorenzo <147382513+byAlexLR@users.noreply.github.com>
2024-10-23 18:14:04 +0100

    Update Ejercicio4.java

--- Programación (PRO)/1. Examen Sorpresa/src/main/java/com/mycompany/examensorpresa/Ejercicio4.java
+++ Programación (PRO)/1. Examen Sorpresa/src/main/java/com/mycompany/examensorpresa/Ejercicio4.java
@@ -15,15 +15,15 @@ public class Ejercicio4 {
         int num1, num2;
         
         Scanner sc = new Scanner(System.in);
-        System.out.print("Número 1: "); //Imprime mensaje del número 1
+        System.out.print("Introduce el numero 1: "); //Imprime mensaje del número 1
         num1 = sc.nextInt(); //Lee el número
-        System.out.print("Número 2: "); //Imprime mensaje del número 2
+        System.out.print("Introduce el numero 2: "); //Imprime mensaje del número 2
         num2 = sc.nextInt(); //Lee el número
         
-        System.out.println("El resultado es:"); //Apartado
+        System.out.println("Los resultados serian:"); //Apartado
         System.out.println("Suma: " + (num1+num2)); 
         System.out.println("Resta: " + (num1-num2));
-        System.out.println("Multiplicación: " + (num1*num2));
-        System.out.println("División: " + (num1/num2));
+        System.out.println("Multiplicacion: " + (num1*num2));
+        System.out.println("Division: " + (num1/num2));
     }
 }
