a7b1682df290d60ffb070004bb619ccb6cfaf4f5
Alex Lorenzo <147382513+byAlexLR@users.noreply.github.com>
2024-10-29 10:08:54 +0000

    Add files via upload

---
+++ Programación (PRO)/Examen 29/src/main/java/com/mycompany/examen29/Ejercicio15.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.examen29;
+
+/**
+ *
+ * @author byAlexLR
+ */
+public class Ejercicio15 {
+
+    public static void main(String[] args) {
+        int num1 = 4, num2 = 10; // Inicio las variables y les establezco el valor a cada una.
+
+        System.out.println("El resultado de las siguientes operaciones utilizando " + num1 + " y " + num2 + " son:"); // Mensaje de inicio, diciendo los números que vamos a usar (Enunciado).
+        System.out.println("La suma: " + (num1 + num2)); // Realizamos la suma y mostramos el resultado.
+        System.out.println("La resta: " + (num1 - num2)); // Realizamos la resta y mostramos el resultado.
+        System.out.println("La multiplicacion: " + (num1 * num2)); // Realizamos la multiplicación y mostramos el resultado.
+        System.out.println("La division: " + (num1 / num2)); // Realizamos la división y mostramos el resultado.
+        System.out.println("El resto: " + (num1 % num2)); // Realizamos el resto y mostramos el resultado.
+    }
+}
