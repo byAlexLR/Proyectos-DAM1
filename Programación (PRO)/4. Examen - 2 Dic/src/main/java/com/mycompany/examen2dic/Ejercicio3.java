95481d8dd7e2eaed6a386d200566783f13ffc46b
Alex Lorenzo <147382513+byAlexLR@users.noreply.github.com>
2024-12-02 19:52:42 +0000

    Update Ejercicio3.java

--- Programación (PRO)/4. Examen - 2 Dic/src/main/java/com/mycompany/examen2dic/Ejercicio3.java
+++ Programación (PRO)/4. Examen - 2 Dic/src/main/java/com/mycompany/examen2dic/Ejercicio3.java
@@ -18,17 +18,16 @@ public class Ejercicio3 {
     }
 
     public static void contador(Scanner sc) { // Método con la variable Scanner importada.
-        int contadormenos = 0, contadormas = 0, num = -1; // Creamos las variables necesarias, contador negativo y otro positivo junto a la variable iniciadora del bucle.
-
-        while (num != 0) { // Bucle While: comprueba que el número sea igual a 0, si no lo es sigue el bucle.
+        int contadormenos = 0, contadormas = 0, num = 0; // Creamos las variables necesarias, contador negativo y otro positivo junto a la variable número.
+        do { // Bucle do-while: se ejecuta al menos una vez.
             System.out.print("Introduzca un número: "); // Mensaje para el usuario, para que introduzca el número.
             num = sc.nextInt(); // Lo lee y almacena en la variable.
             if (num <= 0) { // Comprueba si el número es menor o igual que 0.
-                contadormenos++; // Lo suma al contador de negativo.
+                contadormenos++; // Lo suma al contador de negativos.
             } else {
-                contadormas++; // Lo suma al contador de positivo.
+                contadormas++; // Lo suma al contador de positivos.
             }
-        }
-        System.out.println("El número total de números introducidos es: Positivos = " + contadormas + " / Negativos = " + (contadormenos - 1)); // Lanza el mensaje con el resultado de números introducidos.
+        } while (num != 0); // El bucle continúa mientras el número no sea igual a 0.
+        System.out.println("El número total de números introducidos es: Positivos = " + contadormas + " / Negativos = " + contadormenos); // Lanza el mensaje con el resultado de números introducidos.
     }
 }
